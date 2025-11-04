a2e020aa71a4ae355403ee9b75e42ca2076e6553
Gabrielletrr <gabrielletrr17@gmail.com>
2025-11-04 12:21:57 -0300

    .

---
+++ 011-Cotação pintura.py
@@ -0,0 +1,8 @@
+largura = float(input('Digite a largura em metros da parede: '))
+altura = float(input('Digite a altura em metros da parede: '))
+
+area = largura * altura
+
+q = area/2
+
+print('A quantidade de tinta que vocẽ precisa comprar é {} litros'.format(q))
