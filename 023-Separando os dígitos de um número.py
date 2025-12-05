8f7fe80c2732d661adc1cb84e831c56336111635
Gabrielletrr <gabrielletrr17@gmail.com>
2025-12-05 00:52:16 -0300

    .

---
+++ 023-Separando os dígitos de um número.py
@@ -0,0 +1,8 @@
+num = int(input('Digite um número de 0 a 9999: '))
+n = str(num)
+
+print('-'*50)
+print('Unidade {}'.format(n[3]))
+print('Dezena {}'.format(n[2]))
+print('Centena {}'.format(n[1]))
+print('Milhar {}'.format(n[0]))
